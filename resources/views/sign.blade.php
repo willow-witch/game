fcb206e727b53520657b6e9e3141a76cbb2d2f0a
willow-witch <sealana048@gmail.com>
2022-04-05 13:57:43 +0200

    sign form ver1

--- resources/views/sign.blade.php
+++ resources/views/sign.blade.php
@@ -1,14 +1,33 @@
 <html>
+<head>
+    <link rel="stylesheet" href="/css/reset.css">
+    <link rel="stylesheet" href="/css/app.css">
+</head>
 <body>
-<h1>войти</h1>
 
-<a href="student">
-    student
-</a>
 
-<a href="teacher">
-    teacher
-</a>
+
+<form class="sign-form">
+    <div class="logo-img-big">
+    </div>
+
+    <input type="email" class="sign-input-login" placeholder="E-mail" required>
+
+    <input type="password" class="sign-input-password" placeholder="Пароль" required>
+
+    <div class="sign-btn">
+        <a href="student">
+            student
+        </a>
+    </div>
+
+    <div class="sign-btn">
+        <a href="teacher">
+            teacher
+        </a>
+    </div>
+</form>
+
 
 </body>
 </html>
