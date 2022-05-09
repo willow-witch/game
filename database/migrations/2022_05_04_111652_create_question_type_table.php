f8a214b013be87210f5502c463b96bca8ba8d791
willow-witch <sealana048@gmail.com>
2022-05-09 13:37:01 +0200

    добавлены контроллеры User, Student, Teacher, Admin; данные перенесены в контроллеры

--- database/migrations/2022_05_04_111652_create_question_type_table.php
+++ database/migrations/2022_05_04_111652_create_question_type_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -13,9 +12,9 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('question_type', function (Blueprint $table) {
+        Schema::create('question_type', function(Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string("type", 50);
         });
     }
 
@@ -29,3 +28,4 @@ return new class extends Migration
         Schema::dropIfExists('question_type');
     }
 };
+
